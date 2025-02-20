6b1fafb00afb06cbf36c4c67ba55cadd82d7a1c0
Kai Hendry <hendry@iki.fi>
2025-02-20 16:58:40 +0000

    feat: cmp.Diff via https://dave.cheney.net/2019/05/07/prefer-table-driven-tests
    Thanks Dave!

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.5
 
 require (
 	github.com/apple/pkl-go v0.9.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 )
--- main_test.go
+++ main_test.go
@@ -1,36 +1,47 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func TestRedisBinds(t *testing.T) {
-	// input is a configuration file
-	// output are redis cli commands
-	// assert against expected output
-
-	testCases := []struct {
-		pklConfig             string
-		expectedRedisCommands []string
+	tests := []struct {
+		name                string
+		pklConfig           string
+		expectedRedisBinds  []string
+		expectedErrContains string
 	}{
 		{
+			name:      "normal config with both toggles",
 			pklConfig: "config.pkl",
-			expectedRedisCommands: []string{
+			expectedRedisBinds: []string{
 				"docker exec redis redis-cli -n 6 SET RL:toggle SHADOW",
 				"docker exec redis redis-cli -n 6 SET CB:toggle OFF",
 			},
 		},
+		{
+			name:      "normal config with both toggles",
+			pklConfig: "config2.pkl",
+			expectedRedisBinds: []string{
+				"docker exec redis redis-cli -n 6 SET RL:toggle ON",
+				"docker exec redis redis-cli -n 6 SET CB:toggle ON",
+			},
+		},
 	}
-	for _, tc := range testCases {
-		redisCommands, err := genRedisBinds(tc.pklConfig)
-		if err != nil {
-			t.Errorf("genRedisBinds(%s) failed: %v", tc.pklConfig, err)
-		}
-		if len(redisCommands) != len(tc.expectedRedisCommands) {
-			t.Errorf("genRedisBinds(%s) = %v, want %v", tc.pklConfig, redisCommands, tc.expectedRedisCommands)
-		}
-		for i, redisCommand := range redisCommands {
-			if redisCommand != tc.expectedRedisCommands[i] {
-				t.Errorf("ORDER genRedisBinds(%s) = %v, want %v", tc.pklConfig, redisCommands, tc.expectedRedisCommands)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := genRedisBinds(tt.pklConfig)
+
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
 			}
-		}
+
+			if diff := cmp.Diff(tt.expectedRedisBinds, got); diff != "" {
+				t.Errorf("redis binds mismatch (-want +got):\n%s", diff)
+			}
+		})
 	}
 }
